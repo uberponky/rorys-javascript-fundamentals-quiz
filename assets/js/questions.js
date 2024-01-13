d88a74eb4d0b34da1e83149cb1aa8b4b3fbb2209
Rory Simmonds <rory@blankbox.business>
2024-01-13 23:19:26 +0000

    add questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,27 @@
 var questions = [
   {
-    title: "Test question 1",
-    answers: ["Answer 1a", "Correct", "Answer 3a", "Answer 4a"],
+    title: "How do you access the second element in an array named 'colors'?",
+    answers: ["colors[1]", "colors.index(1)", "colors.get(1)", "colors[2]"],
+    answer: 0
+  },
+  {
+    title: "What does the following function do? function add(a, b) { return a + b; }",
+    answers: ["Prints 'Hello World'", "Adds two numbers and returns the sum", "Stores a value in a variable", "Checks if a number is even"],
     answer: 1
   },
   {
-    title: "Test question 2",
-    answers: ["Answer 1b", "Answer 2b", "Correct", "Answer 4b"],
+    title: "What is the difference between '===' and '=='?",
+    answers: ["No difference, they both do the same thing.", "=== is for numbers, == is for strings.", "== checks for strict equality (value and type), while == only checks for value equality.", "=== is faster, == is slower."],
     answer: 2
   },
   {
-    title: "Test question 3",
-    answers: ["Answer 1", "Correct", "Answer 3", "Answer 4"],
+    title: "What does the console.log() function do?",
+    answers: ["Creates a pop-up window.", "Adds information to the browser's developer tools.", "Changes the text color on the page.", "Runs custom JavaScript code."],
     answer: 1
+  },
+  {
+    title: "What does DOM stand for in JavaScript?",
+    answers: ["Dependant Object Modals, a structure by which modals are ordered by dependency", "Durable Object Maintenance, a methodology of creating self-sustaining objects to reduce workload", "Dangerous Over-Management, the idea that too many top level functions can pollute a codebase", "Document Object Model, the model representing the webpage structure."],
+    answer: 3
   },
 ]
